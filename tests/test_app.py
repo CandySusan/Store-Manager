9e5174e0a3edf11936ee4529d60729ccecd69123
Candy Susan <candysusan55@gmail.com>
2018-10-26 00:39:52 +0300

    Final Commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,7 +18,7 @@ class TestApi(unittest.TestCase):
 
     def test_get_all_products(self):
         response = self.client.get(self.hostname + 'products')
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 200)
 
     def test_post_a_product(self):
         products = {
